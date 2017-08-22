8b277826a871d547853026fca288166faa9e0483
mspriyaa90 <30682132+mspriyaa90@users.noreply.github.com>
2017-08-22 14:18:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/webapp', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
@@ -15,12 +15,12 @@ app.get('/article-one', function(req,res){
 
 app.get('/article-two', function(req,res){
     res.send('Article two requested and will be served');
-})
-;
+});
+
 app.get('/article-three', function(req,res){
     res.send('Article three requested and will be served');
-})
-;
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
